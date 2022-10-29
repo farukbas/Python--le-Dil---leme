f1d51ff0dc41631fd3a67a8c9fbcd932a6906fbd
farukbas <farukbas12345@gmail.com>
2022-10-29 19:32:51 +0300

    texte yazildi test video yüklendi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import moviepy.editor as mp
 
 #Video Dosyası Wav formatına dönüştürülüyor .(video yüklerken uzantisina dikkat edin!)
-clip = mp.VideoFileClip('video_recording.mov')
+clip = mp.VideoFileClip('video_recording4.mp4')
  
 clip.audio.write_audiofile('converted.wav')
 
@@ -12,6 +12,13 @@ audio = sr.AudioFile('converted.wav')
 with audio as source:
   audio_file = r.record(source)
 result = r.recognize_google(audio_file)
+
+#Sonuçlar txt dosyasına yazılıyor..
+with open('kelimeler.txt',mode ='w') as file: 
+   file.write("Tum Kelimeler:") 
+   file.write("\n") 
+   file.write(result) 
+   print("Program tamamlandi!")
 
 
 
