dbc72fdfd53013a2bb42ad8da64f12c0247cf164
farukbas <farukbas12345@gmail.com>
2022-10-29 19:00:38 +0300

    Wav dosyası okundu ve kaydedildi

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import speech_recognition as sr 
 import moviepy.editor as mp
 
-
+#Videonun uzantisina dikkat et!
 clip = mp.VideoFileClip('video_recording.mov')
  
 clip.audio.write_audiofile('converted.wav')
 
+# Wav dosyası google api kullanılarak okunuyor ve kaydediliyor.. 
+r = sr.Recognizer()
+audio = sr.AudioFile('converted.wav')
+with audio as source:
+  audio_file = r.record(source)
+result = r.recognize_google(audio_file)
 
